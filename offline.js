9904beb6ef44edc9857419cf3bf8f5b6e345565f
61131114002 <43134332+61131114002@users.noreply.github.com>
2018-12-01 09:20:24 +0700

    Racing

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1543630309,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/bg-sheet0.png",
+		"images/road-sheet0.png",
+		"images/car-sheet0.png",
+		"images/slow-sheet0.png",
+		"images/pumup-sheet0.png",
+		"images/pumleft-sheet0.png",
+		"images/pumright-sheet0.png",
+		"images/pumdown-sheet0.png",
+		"images/tap-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
